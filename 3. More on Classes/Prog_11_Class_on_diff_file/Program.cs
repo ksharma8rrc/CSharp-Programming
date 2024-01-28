e5e85c3c3d64749c0eb73f0078e6d2ec7c9390d6
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-01-28 05:48:21 -0600

    Create Program.cs

---
+++ 3. More on Classes/Prog_11_Class_on_diff_file/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // memorize to create object
+            Adev obj1 = new Adev();
+            obj1.GetData(50, "ADEV-2008");
+            obj1.PrintData();
+
+            Adev obj2 = new Adev();
+            obj2.GetData(318, "Princess St.");
+            obj2.PrintData();
+
+            Console.ReadKey();
+
+        }
+    }
+}
