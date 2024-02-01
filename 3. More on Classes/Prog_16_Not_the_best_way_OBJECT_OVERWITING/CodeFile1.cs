617e7a52d71ef4426a0f909a58c38533e60d0073
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-02-01 02:09:46 -0600

    Create CodeFile1.cs

---
+++ 3. More on Classes/Prog_16_Not_the_best_way_OBJECT_OVERWITING/CodeFile1.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace RRC.Kushagra.Sharma
+{
+    class Testing
+    {
+        private int x;
+        string y;
+
+        public Testing(int xx, string yy) { 
+        this.x = xx;
+        this.y = yy;
+        }
+
+
+        public void printing()
+        {
+            Console.WriteLine("this is the integer " + x);
+            Console.WriteLine("this is the string: " + y);
+        }
+
+    }
+
+}
