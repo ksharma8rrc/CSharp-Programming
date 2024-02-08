31d88912916f71ee2c9eb80dc4acb22e1a9e1c17
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-02-08 17:50:10 -0600

    Create Program.cs

---
+++ Programming Logic/PL_code_1_ IF_statement/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 20;
+            int y = 18;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
