fdb5b2069d25be42cdec39d90de2aa672c9e941a
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-02-14 00:51:31 -0600

    Create PL_8_loop_break.cs

---
+++ Programming Logic/Loops/PL_8_loop_break.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda", "Hyundai", "Audi" };
+            Console.WriteLine("The length of array is: " + cars.Length);
+
+            for (int i = cars.Length-1 ; i>=0 ; i--)
+            {
+                Console.Write("This is loop cycle: " +i + ": : " + cars[i]);
+                if (cars[i] == "Ford") 
+                { 
+                    Console.WriteLine(" \n \n Can't continue, reached ford "); 
+                    break; 
+                }
+                Console.Write("\nHIT ENTER FOR ANOTHER CYCLE");
+                Console.ReadKey();
+            }
+            Console.WriteLine("\nHIT ENTER TO EXIT PROGRAM \n");
+            Console.ReadKey();
+        }
+    }
+}
