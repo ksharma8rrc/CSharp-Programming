fa407af9c8896409abb63a22d3a2cb072c70302c
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-02-14 00:29:34 -0600

    Create PL_4_Switch.cs

---
+++ Programming Logic/Switch/PL_4_Switch.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the Day Number and i'll tell you what day it is: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-----------------------\n \n");
+            Console.Write("The day is: ");
+
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
