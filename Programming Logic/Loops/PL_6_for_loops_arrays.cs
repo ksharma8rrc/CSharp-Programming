e6b9c5eb8a211d266821c62b453d74125351f44c
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-02-14 00:45:21 -0600

    Create PL_6_for_loops_arrays.cs

---
+++ Programming Logic/Loops/PL_6_for_loops_arrays.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda", "Hyundai", "Audi" };
+            Console.WriteLine("The length of array is: " + cars.Length);
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.Write("This is loop cycle: " +i + ": : " + cars[i]);              
+                Console.Write("\nHIT ENTER FOR ANOTHER CYCLE");
+                Console.ReadKey();
+            }
+            Console.WriteLine("\nHIT ENTER TO EXIT PROGRAM \n");
+            Console.ReadKey();
+
+        }
+    }
+}
