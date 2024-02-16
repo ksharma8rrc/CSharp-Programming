8cd2d43fda9c062eb0fb8f0f6f0aff12e65b6183
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-02-16 03:42:38 -0600

    Update RandomTest.cs

--- Unit Testing/Private Object/RandomTest.cs
+++ Unit Testing/Private Object/RandomTest.cs
@@ -10,9 +10,19 @@ namespace RandomTestProject
         [TestMethod]
         public void TestMethod1()
         {
+            // USING PRIVATE OBJECT
             Random1 random_obj1 = new Random1(3);
             PrivateObject priv = new PrivateObject(random_obj1);
-            Assert.AreEqual((int)priv.GetFieldOrProperty("a"), 3);    
+            Assert.AreEqual((int)priv.GetFieldOrProperty("a"), 3);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // USING PRIVATE OBJECT // UPDATING VALUE OF A TO 1 AND ASSERTING
+            Random1 random_obj1 = new Random1(3);
+            PrivateObject priv = new PrivateObject(random_obj1);
+            priv.SetFieldOrProperty("a", 1);
+            Assert.AreEqual((int)priv.GetFieldOrProperty("a"), 1);
         }
 
         [TestMethod]
