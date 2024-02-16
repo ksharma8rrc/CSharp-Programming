30ddd128e655b7dd31405d892adb80f35237df32
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-02-16 03:15:30 -0600

    Create Program.cs

---
+++ Unit Testing/Private Object/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+
+    public class Random1
+    {
+        private int a;        
+        public Random1(int aa)
+        {
+            this.a = aa;
+        }
+
+        public int squared()
+        {
+            return a*a;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
