d31613d0104edc5048033d6999e071e6d50e126f
ksharma8rrc <157556856+ksharma8rrc@users.noreply.github.com>
2024-02-26 14:12:23 -0600

    Create Program.cs

---
+++ 5. Classes and Inheritance/Sealed Classes/Program.cs
@@ -0,0 +1,45 @@
+//Use the static modifier to declare a static member, which belongs to the type itself rather than to a specific object. The static modifier can be used to declare static classes.
+
+using System;
+
+namespace MyFirstProgram
+    {
+        sealed public class Car
+        {
+            String model;
+            public static int numberOfCars;
+
+            public Car(String model)
+            {
+                this.model = model;
+                numberOfCars++;
+            }
+
+            public static void StartRace()
+            {
+                Console.WriteLine("The race has begun!");
+            }
+        }
+        // abstract promotes inheritance
+        // sealed deters inheritance.
+        public class Car2 : Car // fails to derive when class is sealed. 
+        { }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // static = modifier to declare a static member, which belongs to the class itself
+                //          rather than to any specific object
+
+                Car car1 = new Car("Mustang");
+                Car car2 = new Car("Corvette");
+                Car car3 = new Car("Lambo");
+
+                Console.WriteLine(Car.numberOfCars);
+                Car.StartRace();
+
+                Console.ReadKey();
+            }
+        }
+}
